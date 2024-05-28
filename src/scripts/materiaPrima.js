1ddfcfc05be7608b5dfcdbff0e5522eac6987d90
JhoanSLC <JhoanSLC4@gmail.com>
2024-05-28 12:56:58 -0500

    feat: :sparkles: objeto materias primas

---
+++ src/scripts/materiaPrima.js
@@ -0,0 +1,15 @@
+
+let materiaPrima = {
+    "idMateriaPrima" : "",
+    "nombre" : "",
+    "descripcion" : "",
+    "categoria" : "",
+    "proveedor" : "",
+    "costoPorUnidad" : 0,
+    "unidadDeMedida" : "",
+    "cantidadEnStock" : 0,
+    "fechaDeAdquisicion" : "",
+    "fechaDeVencimiento" : "",
+    "ubicacionEnAlmacen" : "",
+    "notasAdicionales"
+}
